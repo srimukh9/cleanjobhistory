3af42b7088068088b24a9cf72715877988d016de
Yerneni <ysrimukh@38f9d35e4a80.ant.amazon.com>
2020-02-06 11:14:55 -0600

    Initial version of simplejobhistory generated by JHipster-6.6.0

---
+++ src/main/java/com/amazon/simplejobhistory/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.amazon.simplejobhistory.config.audit;
---
+++ src/main/java/com/amazon/simplejobhistory/web/rest/vm/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * View Models used by Spring MVC REST controllers.
+ */
+package com.amazon.simplejobhistory.web.rest.vm;
